f017403e8e4c139538b5cf9dea9eb3ae07420166
amithshubhan <55019776+amithshubhan@users.noreply.github.com>
2023-01-15 21:08:08 +0530

    Update mpiprime.c

--- MPI/mpiprime.c
+++ MPI/mpiprime.c
@@ -3,7 +3,7 @@
 #include <mpi.h>
 
 int isprime(int n){
-	if(n==1) return 0;
+	if(n == 1) return 0;
 	if(n == 2) return 1;
 	for(int i=2;i*i<=n;i++){
 		if(n%i == 0) return 0;
@@ -11,62 +11,33 @@ int isprime(int n){
 	return 1;
 }
 
-int main(int argc,char * argv[]){
-	int ele = 12;
-	// ele = argv[0] - '0';
-	scanf("%d",&ele);
-	int *a = (int *)malloc(sizeof(int)*ele);
-	int *b = (int *)malloc(sizeof(int)*ele);
-	
-	int subsize;
-	int size,rank;
+int main(int argc,char ** argv){
+	int rank,size;
+	int root = 0;
 	MPI_Init(&argc,&argv);
-	
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
+
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	int * ap;
-	int * bp;
-	// printf("rank is %d\n",rank);
-	if(rank == 0){
-		for(int i=0;i<ele;i++)
-		{
-			a[i] = i+1;
-		}
-	}
-	
-	
-
-	 subsize = ele/size;
-	ap = (int *)malloc(sizeof(int)*subsize);
-	bp = (int *)malloc(sizeof(int)*subsize);
-
-	MPI_Bcast(&ele,1,MPI_INT,0,MPI_COMM_WORLD);
-	MPI_Scatter(a,subsize,MPI_INT,ap,subsize,MPI_INT,0,MPI_COMM_WORLD);
-
-	for(int i=0;i<subsize;i++){
-		if(isprime(ap[i])){
-			bp[i] = 1;
-		}else{
-			bp[i] = 0;
-		}
-	}
-
-	MPI_Gather(bp,subsize,MPI_INT,b,subsize,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Status stat;
+	int n;
 
 	if(rank == 0){
-		for(int i=0;i<ele;i++){
-			if(b[i] == 1)
-				printf("%d is prime\n",a[i]);
-			else
-				printf("%d is not prime\n",a[i]);
-
-		}printf("\n");
+		printf("Enter the number\n");
+		scanf("%d",&n);
 	}
+	MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
+	int subsize = (n/size);
+	int ll = (rank*subsize) + 1;
+	int ul = (rank+1)*subsize;
+	if(rank == size-1) ul = n;
+	for(int i=ll;i<=ul;i++){
+		if(isprime(i))
+			printf("Rank %d , number %d is prime\n",rank,i);
+		else
+			printf("Rank %d , number %d is not prime\n",rank,i);
+	}
+
 
 	MPI_Finalize();
-
-
 	return 0;
 }
-
-
