b6087d2733d1fa14ed80446ec0062d4ec94ca20c
amithshubhan <55019776+amithshubhan@users.noreply.github.com>
2023-01-16 06:37:18 +0530

    Update omp4.c

--- omp4.c
+++ omp4.c
@@ -2,15 +2,15 @@
 #include <omp.h>
 #include <stdio.h>
 
-int fibo(int n){
+int fib(int n){
 	if(n < 2) 
 		return n;
 	else{
 		int i,j;
 		#pragma omp task shared(i) firstprivate(n)
-		i = fibo(n-1);
+		i = fib(n-1);
 		#pragma omp task shared(j) firstprivate(n)
-		j = fibo(n-2);
+		j = fib(n-2);
 		#pragma omp taskwait
 		return i+j;
 	}
@@ -18,14 +18,26 @@ int fibo(int n){
 
 int main(){
 
-	int n;
-	printf("Enter the number\n");
-	scanf("%d",&n);
-	#pragma omp parallel shared(n)
-	{
-	#pragma omp single
-	printf("The fibonnaci of %d is %d\n",n,fibo(n));
-	}
+	// int n;
+	// printf("Enter the number\n");
+	// scanf("%d",&n);
+	// #pragma omp parallel for shared(n)
+	
+	// for(int i=1;i<=n;i++){
+	// #pragma omp single
+	// printf("The fibonnaci of %d is %d\n",n,fibo(i));
+// }
+	int n = 20;
+    double start = omp_get_wtime();
+    #pragma omp parallel for
+    for(int i=0;i<n;i++){
+        int t = omp_get_thread_num();
+        printf("thread: %d fib(%d) = %d\n",t,i,fib(i));
+    }
+    double end = omp_get_wtime();
+    printf("using schedule time is : %f\n",end-start);
+
+	
 	
 	return 0;
 }
