0f8a04ea5874562fd7d6ac081ddb195f56c66d83
amithshubhan <55019776+amithshubhan@users.noreply.github.com>
2023-01-16 06:26:03 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
-int fib(int n)
-{
- int i, j;
- if (n<2)
-   return n;
- else
-   {
-      #pragma omp task shared(i) firstprivate(n)
-      i=fib(n-1);
- 
-      #pragma omp task shared(j) firstprivate(n)
-      j=fib(n-2);
- 
-      #pragma omp taskwait
-      return i+j;
-   }
-}
- 
-int main()
-{
- int n = 10;
- 
- omp_set_dynamic(0);
- omp_set_num_threads(4);
- 
- #pragma omp parallel shared(n)
- {
-   #pragma omp single
-   printf ("fib(%d) = %d\n", n, fib(n));
- }
+#include <stdio.h>
+
+int fibo(int n){
+	if(n < 2) 
+		return n;
+	else{
+		int i,j;
+		#pragma omp task shared(i) firstprivate(n)
+		i = fibo(n-1);
+		#pragma omp task shared(j) firstprivate(n)
+		j = fibo(n-2);
+		#pragma omp taskwait
+		return i+j;
+	}
 }
 
-//#pragma omp task shared(i) firstprivate(n)
-//#pragma omp taskwait
+int main(){
+
+	int n;
+	printf("Enter the number\n");
+	scanf("%d",&n);
+	#pragma omp parallel shared(n)
+	{
+	#pragma omp single
+	printf("The fibonnaci of %d is %d\n",n,fibo(n));
+	}
+	
+	return 0;
+}
